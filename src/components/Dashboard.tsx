4394b43f3c1cce4343f277d08f0281243f61993d
CrisPaes30 <cristianpaes90@gmail.com>
2026-05-02 16:20:07 -0300

    Ajuste retorno de navegação1

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect, useRef } from 'react';
 import {
   Box, Typography, IconButton, Chip, Button,
   Dialog, DialogContent, Slide, TextField
